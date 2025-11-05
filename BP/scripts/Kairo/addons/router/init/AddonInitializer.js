b7f855e1fd1d37bf0649eb5784b1555997bad7ed
shizuku86 <shizuku86710@gmail.com>
2025-11-05 14:19:16 +0900

    build

---
+++ BP/scripts/Kairo/addons/router/init/AddonInitializer.js
@@ -0,0 +1,36 @@
+import { system } from "@minecraft/server";
+import { AddonInitializeReceive } from "./AddonInitializeReceive";
+import { AddonInitializeResponse } from "./AddonInitializeResponse";
+export class AddonInitializer {
+    constructor(kairo) {
+        this.kairo = kairo;
+        this.registrationNum = 0;
+        this.receive = AddonInitializeReceive.create(this);
+        this.response = AddonInitializeResponse.create(this);
+    }
+    static create(kairo) {
+        return new AddonInitializer(kairo);
+    }
+    subscribeClientHooks() {
+        system.afterEvents.scriptEventReceive.subscribe(this.receive.handleScriptEvent);
+    }
+    unsubscribeClientHooks() {
+        system.afterEvents.scriptEventReceive.unsubscribe(this.receive.handleScriptEvent);
+    }
+    getSelfAddonProperty() {
+        return this.kairo.getSelfAddonProperty();
+    }
+    refreshSessionId() {
+        return this.kairo.refreshSessionId();
+    }
+    sendResponse() {
+        const selfAddonProperty = this.getSelfAddonProperty();
+        this.response.sendResponse(selfAddonProperty);
+    }
+    setRegistrationNum(num) {
+        this.registrationNum = num;
+    }
+    getRegistrationNum() {
+        return this.registrationNum;
+    }
+}
